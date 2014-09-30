e33c2f814a73d0adf3feb98625fcdf142251da1c
BBraun <bernhard.braun@analytical-software.de>
2014-09-30 15:19:03 +0200

    update of files

--- src/main/java/de/hms/sasunit/sasunitplugin/SASUnitInstallation.java
+++ src/main/java/de/hms/sasunit/sasunitplugin/SASUnitInstallation.java
@@ -1,20 +1,20 @@
 /**
- * 
- *  Copyright (c) 2014, HMS Analytical Software GmbH, Heidelberg
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+* 
+*  Copyright (c) 2014, HMS Analytical Software GmbH, Heidelberg
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 package de.hms.sasunit.sasunitplugin;
 
@@ -37,75 +37,75 @@ import org.kohsuke.stapler.QueryParameter;
 
 @SuppressWarnings("serial")
 public class SASUnitInstallation extends ToolInstallation implements NodeSpecific<SASUnitInstallation>, EnvironmentSpecific<SASUnitInstallation> {
-	
-	 /**
-	* Constructor using fields.
-	*
-	* @param name The name of the SASUnitInstallation
-	* @param home The home folder for this SASUnitInstallation
-	*/
-	@DataBoundConstructor
-	public SASUnitInstallation(String name, String home) {
-		super(name, home, null);
-	}
+   
+   /**
+   * Constructor using fields.
+   *
+   * @param name The name of the SASUnitInstallation
+   * @param home The home folder for this SASUnitInstallation
+   */
+   @DataBoundConstructor
+   public SASUnitInstallation(String name, String home) {
+      super(name, home, null);
+   }
 
-	 /**
-	* Get the installation for the environment.
-	*
-	* @param environment The environment
-	* @return The new installation
-	*/
-	public SASUnitInstallation forEnvironment(EnvVars environment) {
-		return new SASUnitInstallation(getName(), environment.expand(getHome()));
-	}
+   /**
+   * Get the installation for the environment.
+   *
+   * @param environment The environment
+   * @return The new installation
+   */
+   public SASUnitInstallation forEnvironment(EnvVars environment) {
+      return new SASUnitInstallation(getName(), environment.expand(getHome()));
+   }
 
-	public SASUnitInstallation forNode(Node node, TaskListener log) throws IOException, InterruptedException {
-		return new SASUnitInstallation(getName(), translateFor(node, log));
-	}
-	
-	/**
-	* Installation descriptor
-	*/
-	@Extension
-	public static final class DescriptorImpl extends ToolDescriptor<SASUnitInstallation> {
+   public SASUnitInstallation forNode(Node node, TaskListener log) throws IOException, InterruptedException {
+      return new SASUnitInstallation(getName(), translateFor(node, log));
+   }
+   
+   /**
+   * Installation descriptor
+   */
+   @Extension
+   public static final class DescriptorImpl extends ToolDescriptor<SASUnitInstallation> {
 
-	  /**
-		* Check that the path to the SASUnit Installation is specified
-		*
-		* @param value The Folder to check
-		*/
-		public FormValidation doCheckHome(@QueryParameter final File value) {
-			// this can be used to check the existence of a file on the server, so needs to be protected
-			if (Hudson.getInstance().hasPermission(Hudson.ADMINISTER) == false) {
-				return FormValidation.ok();
-			}
-			if ("".equals(value.getPath())) {
-				return FormValidation.ok();
-			}
-			//Warn if folder does not exist on Master
-			if (value.exists() == false) {
-				return FormValidation.warning(Messages.SASUnitPlugInBuilder_PathDoesNotExist());
-			}
-			if (value.isDirectory() == false) {
-				return FormValidation.error(Messages.SASUnitPlugInBuilder_PathIsNoDirectory());
-			}
+      /**
+      * Check that the path to the SASUnit Installation is specified
+      *
+      * @param value The Folder to check
+      */
+      public FormValidation doCheckHome(@QueryParameter final File value) {
+         // this can be used to check the existence of a file on the server, so needs to be protected
+         if (Hudson.getInstance().hasPermission(Hudson.ADMINISTER) == false) {
+            return FormValidation.ok();
+         }
+         if ("".equals(value.getPath())) {
+            return FormValidation.ok();
+         }
+         //Warn if folder does not exist on Master
+         if (value.exists() == false) {
+            return FormValidation.warning(Messages.SASUnitPlugInBuilder_PathDoesNotExist());
+         }
+         if (value.isDirectory() == false) {
+            return FormValidation.error(Messages.SASUnitPlugInBuilder_PathIsNoDirectory());
+         }
 
-			return FormValidation.ok();
-		}
+         return FormValidation.ok();
+      }
 
-	 /**
-		* Check that the name to the SASUnit Installation is set
-		*
-		* @param value The Folder to check
-		*/
-		public FormValidation doCheckName(@QueryParameter final String value) {
-			return FormValidation.validateRequired(value);
-		}
-		
+      /**
+      * Check that the name to the SASUnit Installation is set
+      *
+      * @param value The Folder to check
+      */
+      public FormValidation doCheckName(@QueryParameter final String value) {
+         return FormValidation.validateRequired(value);
+      }
+      
       @Override
-		public String getDisplayName() {
-			return Messages.SASUnitPlugInBuilder_DisplayName();
-		}
+      public String getDisplayName() {
+         return Messages.SASUnitPlugInBuilder_DisplayName();
+      }
 
       @Override
       public SASUnitInstallation[] getInstallations() {
@@ -120,5 +120,6 @@ public class SASUnitInstallation extends ToolInstallation implements NodeSpecifi
       public void setInstallations(SASUnitInstallation... installations) {
          Hudson.getInstance().getDescriptorByType(SASUnitPlugInBuilder.DescriptorImpl.class).setInstallations(installations);
       }
-	}
+   }
 }
+
